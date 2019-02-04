45c9ef23992a0e5ce6e7e742ccad6a876d8a60a6
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 19:26:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -36,4 +36,9 @@ class Pokemon
     WHERE id = ?", hp_hit, @id)
   end
 
+  def alter_hp(new_hp, database_connection)
+  database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+end
+
+
 end
