59c19300d7f51c5fb05b1f22931e42daaf8dd1d2
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 19:26:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,41 +4,66 @@ class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
   @@all = []
 
-  def initialize(id:, name:, type:, db:)
+  def initialize (id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
-    # @hp = hp
     @type = type
+    @hp = hp
     @db = db
-
     @@all << self
   end
 
-  def self.save(name, type, database_connection)
-    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
-
+   def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id, database_connection)
+   def self.find(id, database_connection)
     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
+    hp = pokemon[3]
 
-
-
-    new_poke = Pokemon.new(id: id, name: name, type: type, db: database_connection)
-
+     pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
   end
 
-  # def alter_hp(hp_hit, database_connection)
-  #   database_connection.execute("UPDATE pokemon
-  #   SET hp = ?
-  #   WHERE id = ?", hp_hit, @id)
-  # end
-
-  def alter_hp(new_hp, database_connection)
-  database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
-end
+   def alter_hp(new_hp, database_connection)
+    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+  end
+#   def initialize(id:, name:, type:, db:)
+#     @id = id
+#     @name = name
+#     # @hp = hp
+#     @type = type
+#     @db = db
+#
+#     @@all << self
+#   end
+#
+#   def self.save(name, type, database_connection)
+#     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+#
+#   end
+#
+#   def self.find(id, database_connection)
+#     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+#     name = pokemon[1]
+#     type = pokemon[2]
+#
+#
+#
+#     new_poke = Pokemon.new(id: id, name: name, type: type, db: database_connection)
+#
+#   end
+#
+#   # def alter_hp(hp_hit, database_connection)
+#   #   database_connection.execute("UPDATE pokemon
+#   #   SET hp = ?
+#   #   WHERE id = ?", hp_hit, @id)
+#   # end
+#
+#   def alter_hp(new_hp, database_connection)
+#   database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+# end
 
 
 end
