b09fa4ffc079df0b3186d313e9b18a914a67b825
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 19:25:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,10 +23,10 @@ class Pokemon
     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
-    hp = pokemon[3]
 
 
-    new_poke = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
+
+    new_poke = Pokemon.new(id: id, name: name, type: type, db: database_connection)
 
   end
 
